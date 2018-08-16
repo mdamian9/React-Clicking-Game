32e8d590046b8999991c80a11debc9b9fd31851e
md_dev <m.damian9@hotmail.com>
2018-08-16 16:22:10 -0700

    Added shuffleCards function, shuffling works properly + added comments to Game.js

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,27 +5,56 @@ import cards from "../../cards.json";
 
 class Game extends Component {
 
+    // Setting the state of the Game component: characters is our array of card objects imported form cards.json, user score is set to 0,
+    // and idArr is an array that will hold the id's of every character once they are clicked.
     state = {
         characters: cards,
         score: 0,
-        idArray: []
+        idArr: []
     };
 
+    // shuffleCards function takes in an array and shuffles the elements in the array
+    shuffleCards = (charsArr) => {
+
+        var currentIndex = charsArr.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = charsArr[currentIndex];
+            charsArr[currentIndex] = charsArr[randomIndex];
+            charsArr[randomIndex] = temporaryValue;
+        };
+
+        return charsArr;
+    };
+
+    // matchCharacter function is triggered each time an image of a character is clicked - it takes in an id.
     matchCharacter = (id) => {
 
-        if (this.state.idArray.includes(id)) {
+        // If the id exists in idArr then user has already clicked that character and lost the game. Shuffle cards, clear score and idArr.
+        if (this.state.idArr.includes(id)) {
             this.setState({
+                characters: this.shuffleCards(cards),
                 score: 0,
-                idArray: []
+                idArr: []
             });
         } else {
+            // If the id does not exist in idArr yet, shuffle cards, increase user score by 1, and push the id into idArr.
             this.setState({
+                characters: this.shuffleCards(cards),
                 score: this.state.score + 1
             });
-            this.state.idArray.push(id);
+            this.state.idArr.push(id);
         };
     };
 
+    // Render function that will render our component.
     render() {
         return (
             <div className="container">

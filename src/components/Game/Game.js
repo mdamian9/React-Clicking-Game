3870c7b6435eecf75464fabbec6fc4839c55b45a
md_dev <m.damian9@hotmail.com>
2018-08-16 09:20:53 -0700

    cards.json file working now

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
+import cards from "../../cards.json";
 
 class Game extends Component {
+
+    state = {
+        characters: cards
+    };
+
     render() {
-        return <h1>Hello</h1>;
+        return (
+            <div>
+                {this.state.characters.map(character => (
+                    <p>
+                        {character.name}
+                    </p>
+                ))}
+            </div>
+        );
     };
 };
 
